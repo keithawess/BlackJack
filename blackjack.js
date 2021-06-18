6cb0f8beb6e2e82919553465b1ccbb136d187ad3
keithawess <61606760+keithwess@users.noreply.github.com>
2021-06-18 14:51:12 -0500

    Adds Check score and refill

--- blackjack.js
+++ blackjack.js
@@ -1,5 +1,5 @@
 let deck = [];
-let discard = [];
+let discardPile = [];
 let pHand = [];
 let cHand = [];
 let suitList = ["club", "heart", "diamond", "spade"];
@@ -20,10 +20,6 @@ for(let i = 0; i < 4; i++)
     deck.push({value: 11, face: "ace", suit: suitList[i]})
 }
 
-shuffle(deck);
-
-console.log(deck);
-
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -32,8 +28,52 @@ function shuffle(array) {
 }
 
 function deal(hand){
+    if (deal.length < 10)
+    {
+        refillDeck();
+    }
     hand.push(deck.pop())
 }
 
+function discard(){
+    while (cHand.length > 0)
+    {
+        discardPile.push(cHand.pop());
+    }
+    while (pHand.length > 0)
+    {
+        discardPile.push(pHand.pop());
+    }
+}
+
+function refillDeck()
+{
+    while (discardPile.length > 0)
+    {
+        deck.push(discardPile.pop());
+    }
+    shuffle(deck);
+}
+
+function checkScore(hand) {
+    let total = 0;
+    for(let i = 0; i < hand.length; i++)
+    {
+        total += hand[i].value;
+    }
+    return total;
+}
+
 deal(pHand);
-console.log(deck, pHand);
+deal(pHand);
+
+deal(cHand);
+deal(cHand);
+
+console.log(pHand);
+console.log(cHand);
+
+console.log(checkScore(pHand));
+console.log(checkScore(cHand));
+
+console.log(pHand);
